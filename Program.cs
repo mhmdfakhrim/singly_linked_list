cb6c34d592345228c684423bb96921485190c3e8
mhmdfakhrim <muhfakhri4@gmail.com>
2022-11-07 17:18:27 +0700

    latihan

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace singly_linked_list
     class list
     {
         Node START;
-        
+
         public list()
         {
             START = null;
@@ -31,11 +31,11 @@ namespace singly_linked_list
             newnode.name = nm;
             if (START == null || rollNo <= START.rollnumber)
             {
-                if((START != null) &&(rollNo == START.rollnumber))
+                if ((START != null) && (rollNo == START.rollnumber))
                 {
                     Console.Writeline();
                     return;
-                }    
+                }
                 newnode.next = START;
                 START = newnode;
                 return;
@@ -45,14 +45,29 @@ namespace singly_linked_list
             previous = START;
             current = START;
 
-            while ((current != null)&&(rollNo == current.rollnumber))
+            while ((current != null) && (rollNo == current.rollnumber))
             {
-                if(rollNo == current.rollnumber)
+                if (rollNo == current.rollnumber)
                 {
-                    Console.Writeline
+                    Console.Writeline();
+                    return;
                 }
+                previous.next = current;
+                previous.next = newnode;
             }
-
+            newnode.next = current;
+            previous.next = newnode;
+        }
+        public bool delNode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (Search(rollNo, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == START)
+                START = START.next;
+            return true;
         }
     }
 
