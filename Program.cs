6aedb3471bdc3648e16b0a15bc6633de02100843
mhmdfakhrim <muhfakhri4@gmail.com>
2022-11-07 16:49:31 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace singly_linked_list
+{
+    //each node consist of the information part and link to the next node
+    class Node
+    {
+        public int rollnumber;
+        public string name;
+        public Node next;
+    }
+
+    class list
+    {
+
+    }
+
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
